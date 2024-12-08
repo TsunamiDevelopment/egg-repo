372cd363ad4fe4df02bf8dd66442ceb66ff4f5a2
Cam <chrlew082@gmail.com>
2024-12-08 16:34:35 -0700

    compression & warning

--- utils/DownloadFile.js
+++ utils/DownloadFile.js
@@ -3,21 +3,15 @@ const axios = require("axios");
 const { once } = require("events");
 
 async function downloadFile(fileUrl, outputLocationPath) {
-  const writer = fs.createWriteStream(outputLocationPath);
-
-  try {
-    const response = await axios.get(fileUrl, { responseType: "stream" });
-
-    response.data.pipe(writer);
-
-    // Wait for the stream to finish
-    await once(writer, "finish");
-
-    return true; // Indicate success
-  } catch (error) {
-    writer.close(); // Close the writer if an error occurs
-    throw error;    // Rethrow the error to the caller
-  }
+	const writer = fs.createWriteStream(outputLocationPath);
+	try {
+    	const response = await axios.get(fileUrl, { responseType: "stream" });
+    	response.data.pipe(writer);
+    	await once(writer, "finish");
+    	return true;
+  	} catch (error) {
+    	writer.close();
+	    throw error;
+	}
 }
-
 module.exports = { downloadFile }
