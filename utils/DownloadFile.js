e94abf43bf5b855a0c3067863aaa5336356f6988
Cam <chrlew082@gmail.com>
2024-12-16 10:11:17 -0700

    update downloading files to actually respect await

--- utils/DownloadFile.js
+++ utils/DownloadFile.js
@@ -1,17 +1,27 @@
 const fs = require("fs");
 const axios = require("axios");
-const { once } = require("events");
 
 async function downloadFile(fileUrl, outputLocationPath) {
-	const writer = fs.createWriteStream(outputLocationPath);
-	try {
-    	const response = await axios.get(fileUrl, { responseType: "stream" });
-    	response.data.pipe(writer);
-    	await once(writer, "finish");
-    	return true;
-  	} catch (error) {
-    	writer.close();
-	    throw error;
-	}
+  return new Promise(async (resolve, reject) => {
+    const writer = fs.createWriteStream(outputLocationPath);
+
+    try {
+      const response = await axios.get(fileUrl, { responseType: "stream" });
+      response.data.pipe(writer);
+
+      // Listen for the 'finish' event and resolve the promise
+      writer.on("finish", () => resolve(true));
+
+      // Handle stream errors
+      writer.on("error", (error) => {
+        writer.close();
+        reject(error);
+      });
+    } catch (error) {
+      writer.close();
+      reject(error);
+    }
+  });
 }
-module.exports = { downloadFile }
+
+module.exports = { downloadFile };
