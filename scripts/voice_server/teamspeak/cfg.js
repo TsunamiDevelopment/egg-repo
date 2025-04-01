3e9595fedbb79bf44ec75d004446866322abcc22
Cam <cam@expx.dev>
2025-04-01 15:18:56 -0600

    fix(v-t): specify path to ini file

--- scripts/voice_server/teamspeak/cfg.js
+++ scripts/voice_server/teamspeak/cfg.js
@@ -8,6 +8,6 @@ module.exports = {
 		"system": "system",
 		"version": null,
 		// %%__VAR__%% are launcher-replaced flags, {{VAR}} are installer-replaced flags
-		"cmd": `/home/container/ts3server license_accepted=1 inifile=ts3server.ini`
+		"cmd": `/home/container/ts3server license_accepted=1 inifile=/home/container/ts3server.ini`
 	}
 }
