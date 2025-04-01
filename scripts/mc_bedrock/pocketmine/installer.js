4949c91b9755b311261af251dfbf9e6e4b33eb2d
Cam <cam@expx.dev>
2025-04-01 14:23:03 -0600

    feat(mc-b-pmmp): auto-write server properties

--- scripts/mc_bedrock/pocketmine/installer.js
+++ scripts/mc_bedrock/pocketmine/installer.js
@@ -1,8 +1,33 @@
+const fs = require("fs");
+
 module.exports = async function create(version) {
 	const { execSync } = require("child_process");
 	execSync("bash -c 'cd /home/container && curl -sL https://get.pmmp.io | bash -s -'", { stdio: "inherit" });
 
 	const cmd = require('./cfg').runner.cmd
+
+	const properties = [
+		`language=eng`,
+		`motd=MultiEgg Hosted PocketMine-MP Server`,
+		`server-port=${process.env.SERVER_PORT}`,
+		'server-portv6=0',
+		'max-players=20',
+		'view-distance=20',
+		'white-list=off',
+		'enable-query=on',
+		'enable-ipv6=off',
+		'force-gamemode=off',
+		'hardcore=off',
+		'pvp=on',
+		'difficulty=2',
+		'generator-settings=',
+		'level-name=world',
+		'level-seed=',
+		'level-type=default',
+		'auto-save=on',
+		'xbox-auth=on',
+	].join("\n");
+	fs.writeFileSync("/home/container/server.properties", properties, { encoding: "utf8" });
 
 	return {
 		sdk: {
