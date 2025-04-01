cb8b8d819821b0d1782fb6e76f1d974c49dec3d3
Cam <cam@expx.dev>
2025-04-01 15:06:35 -0600

    fix: update path x2

--- scripts/voice_server/teamspeak/installer.js
+++ scripts/voice_server/teamspeak/installer.js
@@ -15,7 +15,7 @@ module.exports = async function create(version) {
 
 	// Extract the downloaded file
 	const tarStream = fs.createReadStream("/home/container/teamspeak.tar.bz2").pipe(bz2());
-	tarStream.pipe(tarfs.extract("/home/container/teamspeak3-server_linux_x86"));
+	tarStream.pipe(tarfs.extract("/home/container/teamspeak3"));
 
 	await new Promise((resolve, reject) => {
 		tarStream.on("end", () => {
@@ -29,7 +29,7 @@ module.exports = async function create(version) {
 	});
 	
 	// move everything from the extracted folder to the current directory
-	const extractedDir = "/home/container/teamspeak3-server_linux_amd64/";
+	const extractedDir = "/home/container/teamspeak3/";
 	const files = fs.readdirSync(extractedDir);
 	files.forEach((file) => {
 		const oldPath = `${extractedDir}${file}`;
