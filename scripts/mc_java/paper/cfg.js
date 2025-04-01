3b992b4ee25a3500363e95971f543fc17b8a2332
Cam <cam@expx.dev>
2025-04-01 13:15:52 -0600

    test: async not top level

--- scripts/mc_java/paper/cfg.js
+++ scripts/mc_java/paper/cfg.js
@@ -1,13 +1,15 @@
-const { allVersions, parseVersion, versionJava, versionsMinecraft } = require("../../../utils/McJarsApp");
+import { versionJava, versionsMinecraft } from "../../../utils/McJarsApp";
 
-module.exports = {
+const config = {
 	"id": "mc_java_paper",
 	"friendly_name": "PaperMC",
-	"versions": await versionsMinecraft("paper"),
+	"versions": await (async () => await versionsMinecraft("paper"))(),
 	"runner": {
 		"system": "java",
 		"version": function(id) { return versionJava(id); },
 		// %%__VAR__%% are launcher-replaced flags, {{VAR}} are installer-replaced flags
 		"cmd": `/home/container/.sdkman/candidates/java/{{JDK_VERSION}}/bin/java %%__BEFOREFLAGS__%% -jar /home/container/server-{{VERSION}}.jar`
 	}
-}
+};
+
+module.exports = config;
